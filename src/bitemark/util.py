289cae13c7fd3e79e2d4df163defe8ceb6cc29ad
Thomas <t4k1t+dev@protonmail.com>
2026-04-24 19:42:27 +0200

    Refactor
    Signed-off-by: Thomas <t4k1t+dev@protonmail.com>

---
+++ src/bitemark/util.py
@@ -0,0 +1,38 @@
+"""BiteMark utility functions."""
+
+import re
+from pathlib import Path
+
+
+def extract_recipes_from_file(filepath: Path) -> list[str]:
+    with Path.open(filepath, encoding="utf-8") as f:
+        lines = f.readlines()
+
+    recipes = []
+    current_recipe = []
+    in_recipe = False
+    recipe_header_level = None
+
+    header_pattern = re.compile(r"^(#{1,5})\s+")
+    for line in lines:
+        match = header_pattern.match(line)
+        if match:
+            level = len(match.group(1))
+            if not in_recipe:
+                in_recipe = True
+                recipe_header_level = level
+                current_recipe = [line.rstrip("\n")]
+                continue
+
+            elif level <= recipe_header_level:
+                if current_recipe:
+                    recipes.append("\n".join(current_recipe))
+                recipe_header_level = level
+                current_recipe = [line.rstrip("\n")]
+                continue
+
+        if in_recipe:
+            current_recipe.append(line.rstrip("\n"))
+    if current_recipe:
+        recipes.append("\n".join(current_recipe))
+    return recipes
